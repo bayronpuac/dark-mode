bd509f3b0a2175ccacea5a8dfe6a0f1260962013
Bayron Puac <bayronpuac@gmail.com>
2019-10-02 16:33:20 -0500

    Added the new file and was able to have the code working.

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react'; 
+
+
+export const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage('dark-mode', initialValue)
+
+     useEffect(() => {
+         darkMode === true ? document.querySelector('body').classList.add('dark-mode') :
+         document.querySelector('body').classList.remove('dark-mode');
+     });
+
+     return [darkMode, setDarkMode];
+}
